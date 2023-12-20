b8f4af89bb37ecdce389c5875ee48be267c1578f
Usborn Ocampo <usborn116@gmail.com>
2023-12-20 15:40:47 -0800

    updated README

---
+++ README.md
@@ -0,0 +1,7 @@
+Playing with Django to see if I can get a trade app going!
+
+Resources:
+- Model field reference: https://docs.djangoproject.com/en/5.0/ref/models/fields/#model-field-types
+- Python String interpolation: https://www.programiz.com/python-programming/string-interpolation#google_vignette
+- Running Flask apps: https://stackoverflow.com/questions/31252791/flask-importerror-no-module-named-flask
+- 
--- trade_app/models.py
+++ trade_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class League(models.Model):
+
+    name = models.CharField(max_length=50)
+    year = models.IntegerField()
+    id = models.IntegerField()
+
+
+class Team(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    id = models.IntegerField()
+
+
+
+class Player(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+class PlayerStatCard(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+class LeagueStatCard(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
